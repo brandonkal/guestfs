2e52a158c899f78d0def1311a2638d506303d868
Brandon Kalinowski <brandon@brandonkalinowski.com>
2018-09-19 16:57:13 -0700

    entrypoint expansion
    Also switched to /mnt/user/domains to match unraid

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 #### Validate input ####
-if [[ -f /mnt/domains/"$1"/vdisk1.img ]]; then
-  echo "Error: File not found." >&2
+if [[ -f "/mnt/user/domains/$1/vdisk1.img" ]]; then
+  echo "Error: File /mnt/user/domains/$1/vdisk1.img not found." >&2
   echo "Usage: docker run ... Cloud 10 to customize the Cloud/vdisk1.img file and resize it to 10GBs." >&2
   exit 1
 fi
@@ -13,5 +13,5 @@ if [[ ! "$2" || "$2" = *[^0-9]* ]]; then
     exit 1
 fi
 
-qemu-img resize /mnt/domains/"$1"/disk1.img "$2"G
-virt-customize -a /mnt/domains/"$1"/vdisk1.img --uninstall cloud-init --ssh-inject root:file:/root/root.pubkey --hostname "$1"
+qemu-img resize "/mnt/user/domains/$1/disk1.img" "$2"G
+virt-customize -a "/mnt/user/domains/$1/vdisk1.img" --uninstall cloud-init --ssh-inject root:file:/root/root.pubkey --hostname "$1"
