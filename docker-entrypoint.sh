94ee364ff754f0ddf4caf0e0d6f4ea1a41ceb8c0
Brandon Kalinowski <brandon@brandonkalinowski.com>
2018-09-19 17:56:12 -0700

    Fix script and add default disk size logic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 #### Validate input ####
-if [[ -f "/mnt/user/domains/$1/vdisk1.img" ]]; then
+if [[ ! -f "/mnt/user/domains/$1/vdisk1.img" ]]; then
   echo "Error: File /mnt/user/domains/$1/vdisk1.img not found." >&2
   echo "Usage: docker run ... Cloud 10 to customize the Cloud/vdisk1.img file and resize it to 10GBs." >&2
   exit 1
+fi
+
+if [[ -z "$2" ]]; then
+  size="10"
+else
+  size="$2"
 fi
 
 if [[ ! "$2" || "$2" = *[^0-9]* ]]; then
@@ -13,5 +19,5 @@ if [[ ! "$2" || "$2" = *[^0-9]* ]]; then
     exit 1
 fi
 
-qemu-img resize "/mnt/user/domains/$1/disk1.img" "$2"G
+qemu-img resize "/mnt/user/domains/$1/disk1.img" "$size"G
 virt-customize -a "/mnt/user/domains/$1/vdisk1.img" --uninstall cloud-init --ssh-inject root:file:/root/root.pubkey --hostname "$1"
